257afacff6a1a14e8d3641b94dd8745269030ac3
Ujjawal Singh <155844416+the-ujjawal-singh@users.noreply.github.com>
2024-10-04 23:01:52 +0530

    Add files via upload

---
+++ keyboard/script.js
@@ -0,0 +1,21 @@
+const insert  = document.getElementById('insert')
+
+window.addEventListener('keydown', (e)=>{
+    insert.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+        <th>Key</th>
+        <th>Keycode</th>
+        <th>Code</th>
+    <tr>
+        <td>${e.key === " " ? "Space" : e.key}</td>
+        <td>${e.keyCode}</td>
+        <td>${e.code}</td>
+    </tr>
+    <tr>
+    </table>
+        </div>   
+
+    `
+})
